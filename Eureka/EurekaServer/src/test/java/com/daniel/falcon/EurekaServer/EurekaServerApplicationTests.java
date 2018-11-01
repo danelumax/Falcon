47fcaeaf76ad8682b8ba55f325bd3133fc154208
chend19 <Daniel.L.Chen@emc.com>
2018-11-01 17:21:29 +0800

    create new folder to put Eureka sub-project

--- Eureka/EurekaServer/src/test/java/com/daniel/falcon/EurekaServer/EurekaServerApplicationTests.java
+++ Eureka/EurekaServer/src/test/java/com/daniel/falcon/EurekaServer/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.daniel.Falcon;
+package com.daniel.falcon.EurekaServer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FalconApplicationTests {
+public class EurekaServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
