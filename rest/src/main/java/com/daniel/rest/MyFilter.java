1a8e1e4f4d730c1156cc482232be32987bced13b
danelumax <474198699@qq.com>
2019-01-28 13:33:43 +0800

    add raw log framework

--- rest/src/main/java/com/daniel/rest/MyFilter.java
+++ rest/src/main/java/com/daniel/rest/MyFilter.java
@@ -1,5 +1,8 @@
 package com.daniel.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +11,8 @@ import java.io.IOException;
 
 @WebFilter(urlPatterns = "/*")
 public class MyFilter implements Filter {
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -17,6 +22,7 @@ public class MyFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+        log.info("enter in entityParam");
         filterChain.doFilter(request, response);
     }
 
